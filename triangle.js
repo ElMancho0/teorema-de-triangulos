2cd901b47c55c4b8ca0b7f1528efb853ec9b517f
Alana2612 <141462300+Alana2612@users.noreply.github.com>
2024-03-11 13:40:31 -0600

    archivo js

--- triangle.js
+++ triangle.js
@@ -74,27 +74,3 @@ function calcularTriangulo(event) {
         </div>
     `;
 }
-document.addEventListener('DOMContentLoaded', function () {
-    const inputs = document.querySelectorAll('input[type="number"]');
-    inputs.forEach(input => {
-        input.addEventListener('input', function () {
-            const ladoA = parseFloat(document.getElementById('sideA').value);
-            const ladoB = parseFloat(document.getElementById('sideB').value);
-            const ladoC = parseFloat(document.getElementById('sideC').value);
-            if (!isNaN(ladoA) && !isNaN(ladoB) && !isNaN(ladoC)) {
-                calcularTriangulo(event);
-            }
-        });
-    });
-
-    inputs.forEach(input => {
-        input.addEventListener('input', function () {
-            const ladoA = parseFloat(document.getElementById('sideA').value);
-            const ladoB = parseFloat(document.getElementById('sideB').value);
-            const ladoC = parseFloat(document.getElementById('sideC').value);
-            if (isNaN(ladoA) || isNaN(ladoB) || isNaN(ladoC)) {
-                document.getElementById('resultado').innerHTML = '';
-            }
-        });
-    });
-});
